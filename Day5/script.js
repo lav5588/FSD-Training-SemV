38ca21ba1de49a137b8b0e1dd2c48aa431c3be76
Lav5588 <lavkumaryadavanshu@gmail.com>
2024-10-15 15:36:30 +0530

    Final commit on day5

---
+++ Day5/script.js
@@ -0,0 +1,23 @@
+// let x = confirm('Are you sure to Logout')
+// alert(x)
+
+// let v = prompt("Enter your Name: ","Lav Kumar Yadav")
+// alert(v)
+
+// let displayName = ()=>{
+//     console.log("Welcome to Arrow Function in JavaScript");
+// }
+// displayName()
+
+// let say = ()=> console.log("Single line arrow function");
+// say()
+
+
+
+// let sum = (x,y) => {
+//     return x+y;
+// }
+// let res = sum(12,32);
+// console.log(res);
+
+
